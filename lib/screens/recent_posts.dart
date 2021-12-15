1868ed7f6cac920488e402e708311625f0ddaea9
drewosmond <drewosmond@gmail.com>
2021-12-14 21:31:01 -0800

    created new screen to display a specific reddit post when clicked

--- lib/screens/recent_posts.dart
+++ lib/screens/recent_posts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import './reddit_post.dart';
 
 class RecentPosts extends StatefulWidget {
   const RecentPosts({Key? key}) : super(key: key);
@@ -53,10 +54,12 @@ class _RecentPostsState extends State<RecentPosts> {
     for (var post in responsePosts) {
       Map<String, dynamic> currPost = post['data'];
 
-      newPosts.add(Post(
-          title: currPost['title'],
-          text: currPost['selftext'],
-          upvotes: currPost['ups']));
+      newPosts.add(
+        Post(
+            title: currPost['title'],
+            text: currPost['selftext'],
+            upvotes: currPost['ups']),
+      );
     }
 
     posts.addAll(newPosts);
@@ -65,16 +68,32 @@ class _RecentPostsState extends State<RecentPosts> {
 
   Widget _renderPosts() {
     return Scrollbar(
-      child: ListView.builder(
+      child: ListView.separated(
         itemCount: posts.length,
         itemBuilder: (context, index) => _renderTile(context, posts[index]),
+        separatorBuilder: (BuildContext context, int index) => const Divider(
+          color: Colors.deepOrange,
+        ),
       ),
     );
   }
-}
 
-Widget _renderTile(BuildContext context, Post post) {
-  return ListTile(title: Text(post.title));
+  void _handleTap(BuildContext context, Post post) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => RedditPost(post: post)));
+  }
+
+  Widget _renderTile(BuildContext context, Post post) {
+    return Container(
+      padding: const EdgeInsets.all(24.0),
+      child: ListTile(
+        title: Text(post.title),
+        onTap: () => _handleTap(context, post),
+        // onTap: Navigator.push(context,
+        //     MaterialPageRoute(builder: (context) => RedditPost(post: post))),
+      ),
+    );
+  }
 }
 
 class Post {
@@ -83,93 +102,4 @@ class Post {
   final int upvotes;
 
   Post({required this.text, required this.title, required this.upvotes});
-}
-
-// import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final Future<String> _calculation = Future<String>.delayed(
-    const Duration(seconds: 2),
-    () => 'Data Loaded',
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.headline2!,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future: _calculation, // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              const Icon(
-                Icons.check_circle_outline,
-                color: Colors.green,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Result: ${snapshot.data}'),
-              )
-            ];
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              )
-            ];
-          } else {
-            children = const <Widget>[
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Awaiting result...'),
-              )
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        },
-      ),
-    );
-  }
 }
