49754ae0ffcffea7c7befb4765ff71b293efa4e9
drewosmond <drewosmond@gmail.com>
2021-12-14 23:06:47 -0800

    completed conditional rendering for images

--- lib/controllers/images.dart
+++ lib/controllers/images.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class Images {
   static renderImage(String url) {
-    if (url.endsWith(".jpg") ||
-        url.endsWith(".jpg") ||
-        url.contains("https://gfycat.com")) {
+    if (url.endsWith(".jpg") || url.endsWith(".jpg")) {
       print(url);
       return Image.network(url);
     }
+
+    // if (url.contains("https://gfycat.com")) {
+    //   return Image.network(url + "-mobile.mp4");
+    // }
 
     return null;
   }
